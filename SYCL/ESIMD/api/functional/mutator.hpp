7cc2f674c94df0416edeeb133af0de198829cfb6
Vasily Trikolich <vasilyx.tricolich@intel.com>
2022-02-01 12:57:45 +0300

    [SYCL][ESIMD] Update logic for mutator functors and update comments

--- SYCL/ESIMD/api/functional/mutator.hpp
+++ SYCL/ESIMD/api/functional/mutator.hpp
@@ -22,56 +22,63 @@
 
 namespace esimd_test::api::functional {
 
-// Provides base functions to mutate value.
+// In some test cases it’s possible to pass a reference data as the input data
+// directly without any modification. For example, if we check some copy
+// constructor or memory move operation it’s OK to have any data values as the
+// input ones. But in most cases we need to consider the possibility of UB for
+// C++ operations due to modification of input values.
+//
+// Mutation mechanism covers such requirement.
+// The mutator namespace is intended to store such mutators alongside with
+// the generic ones provided below.
 namespace mutator {
 
-template <typename T> class Add {
+// Replace specific reference values to the bigger ones, so we can safely
+// substract `val` later.
+template <typename T> class For_subtraction {
   T m_value;
 
 public:
-  Add(T value_for_mutation) : m_value(value_for_mutation) {}
+  For_subtraction(T val)
+      : m_value((assert(val > 0 && "Invalid value."), val)) {}
 
-  void operator()(T &value_for_mutation) {
+  void operator()(T &val) {
     if constexpr (std::is_signed_v<T>) {
-      if (value<T>::max() - m_value <= value_for_mutation) {
-        value_for_mutation = value<T>::max();
-      } else {
-        value_for_mutation += m_value;
+      if (val < value<T>::lowest() + m_value) {
+        val = value<T>::lowest() + m_value;
       }
-    } else {
-      value_for_mutation += m_value;
     }
   }
 };
 
-template <typename T> class Sub {
+// Replace specific reference values to the smaller ones, so we can safely add
+// `val` later.
+template <typename T> class For_addition {
   T m_value;
 
 public:
-  Sub(T value_for_mutation) : m_value(value_for_mutation) {}
+  For_addition(T val) : m_value((assert(val > 0 && "Invalid value."), val)) {}
 
-  void operator()(T &value_for_mutation) {
+  void operator()(T &val) {
     if constexpr (std::is_signed_v<T> || std::is_same_v<T, sycl::half>) {
-      if (value<T>::lowest() + m_value >= value_for_mutation) {
-        value_for_mutation = value<T>::lowest();
-      } else {
-        value_for_mutation -= m_value;
+      if (val > value<T>::max() - m_value) {
+        val = value<T>::max() - m_value;
       }
-    } else {
-      value_for_mutation -= m_value;
     }
   }
 };
 
-template <typename T> class Divide {
+// Replace specific reference values to the divided ones, so we can safely
+// multiply to `val` later.
+template <typename T> class For_division {
   T m_value;
 
 public:
-  Divide(T value_for_mutation) : m_value(value_for_mutation) {}
+  For_division(T val) : m_value((assert(val > 0 && "Invalid value."), val)) {}
 
-  void operator()(T &value_for_mutation) {
-    if (value_for_mutation != value<T>::denorm_min()) {
-      value_for_mutation /= m_value;
+  void operator()(T &val) {
+    if (val != value<T>::denorm_min()) {
+      val /= m_value;
     }
   }
 };
